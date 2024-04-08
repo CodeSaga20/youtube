1d1ffb76966cbc4cba7e167fa5edebfefb5f1265
oyashi <khushigoyal2022@gmail.com>
2024-04-08 11:01:06 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// var tl= gsap.timeline()
+// tl.from("h4",{
+//     y: -50,
+//     opacity:0,
+//     duration: 1,
+//     delay:0.4,
+//     stagger:0.8
+// })
+
+gsap.from("h4",{
+    y: -50,
+    opacity:0,
+    duration: 1,
+    delay:0.4,
+    stagger:1
+})
